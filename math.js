826190e3ac045501fb3287cea35ec98eb13fd6db
Sabin <sabinanwar6@gmail.com>
2025-04-06 21:33:43 +0200

    Refactor math.js to replace random integer generation with a recursive function for logging numbers

--- math.js
+++ math.js
@@ -1,11 +1,16 @@
-var array=[]
+// var array=[]
 
-for(let i = 0; i <= 10; i++){
-    var number = Math.random() * 10+1;
-    var randomWithoutDecimal = Math.floor(number);
-    console.log(number);
+// for(let i = 0; i <= 10; i++){
+//     var number = Math.random() * 10+1;
+//     var randomWithoutDecimal = Math.floor(number);
+//     console.log(number);
 
-    console.log(randomWithoutDecimal);
-    array.push(randomWithoutDecimal);
+//     console.log(randomWithoutDecimal);
+//     array.push(randomWithoutDecimal);
+// }
+// console.log(array);
+function randomNumber(a) {
+ if(a++<100) randomNumber(a);
+ console.log(a);
 }
-console.log(array);
+randomNumber(0);
